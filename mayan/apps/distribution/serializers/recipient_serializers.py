95900ea0d99925394f185c593a2b10c675c6b562
Vitt7 <vitt7@example.com>
2025-10-18 18:10:17 +0300

    feat: Refactor recipient serializers and enhance recipient management UI
    - Update RecipientSerializer and RecipientListSerializer to use ModelSerializer for improved data handling.
    - Remove unnecessary URL fields and add logic to handle extra data during create and update operations.
    - Revamp recipient list template to include a modal for creating and editing recipients, enhancing user interaction.
    - Implement JavaScript functions for managing recipients, including fetching, editing, and deleting, to streamline the user experience.

--- mayan/apps/distribution/serializers/recipient_serializers.py
+++ mayan/apps/distribution/serializers/recipient_serializers.py
@@ -1,32 +1,29 @@
 from django.utils.translation import ugettext_lazy as _
 
-from mayan.apps.rest_api import serializers
+from rest_framework import serializers
 
 from ..models import Recipient, RecipientList
 
 
-class RecipientSerializer(serializers.HyperlinkedModelSerializer):
-    recipient_lists_url = serializers.HyperlinkedIdentityField(
-        lookup_url_kwarg='recipient_id',
-        view_name='rest_api:recipient-list'
-    )
-
+class RecipientSerializer(serializers.ModelSerializer):
     class Meta:
-        extra_kwargs = {
-            'url': {
-                'lookup_url_kwarg': 'recipient_id',
-                'view_name': 'rest_api:recipient-detail'
-            }
-        }
         fields = (
             'id', 'email', 'name', 'organization', 'locale',
-            'created', 'modified', 'url', 'recipient_lists_url'
+            'created', 'modified'
         )
         model = Recipient
         read_only_fields = ('id', 'created', 'modified')
 
+    def create(self, validated_data):
+        validated_data.pop('_instance_extra_data', None)
+        return super().create(validated_data)
 
-class RecipientListSerializer(serializers.HyperlinkedModelSerializer):
+    def update(self, instance, validated_data):
+        validated_data.pop('_instance_extra_data', None)
+        return super().update(instance, validated_data)
+
+
+class RecipientListSerializer(serializers.ModelSerializer):
     recipients = RecipientSerializer(many=True, read_only=True)
     recipients_id_list = serializers.ListField(
         child=serializers.IntegerField(),
@@ -40,21 +37,16 @@ class RecipientListSerializer(serializers.HyperlinkedModelSerializer):
     )
 
     class Meta:
-        extra_kwargs = {
-            'url': {
-                'lookup_url_kwarg': 'recipient_list_id',
-                'view_name': 'rest_api:recipientlist-detail'
-            }
-        }
         fields = (
             'id', 'name', 'owner', 'recipients', 'internal_users',
             'recipients_id_list', 'internal_users_id_list',
-            'created', 'modified', 'url'
+            'created', 'modified'
         )
         model = RecipientList
         read_only_fields = ('id', 'created', 'modified')
 
     def create(self, validated_data):
+        validated_data.pop('_instance_extra_data', None)
         recipients_id_list = validated_data.pop('recipients_id_list', [])
         internal_users_id_list = validated_data.pop('internal_users_id_list', [])
 
@@ -73,8 +65,9 @@ class RecipientListSerializer(serializers.HyperlinkedModelSerializer):
         return instance
 
     def update(self, instance, validated_data):
-        recipients_id_list = validated_data.pop('recipients_id_list', [])
-        internal_users_id_list = validated_data.pop('internal_users_id_list', [])
+        validated_data.pop('_instance_extra_data', None)
+        recipients_id_list = validated_data.pop('recipients_id_list', None)
+        internal_users_id_list = validated_data.pop('internal_users_id_list', None)
 
         instance = super().update(instance, validated_data)
 
